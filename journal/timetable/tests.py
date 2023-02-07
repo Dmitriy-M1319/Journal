69952f4c684273b557c904b1ea6c536e08ec2714
Dmitriy-M1319 <dimon.5642014@yandex.ru>
2023-02-07 17:52:17 +0300

    Start to creating the unit tests

--- journal/timetable/tests.py
+++ journal/timetable/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from datetime import date, datetime
+
+from .timetable_service import getDateAndTimeFromStr, getDateFromStr
 
 # Create your tests here.
+class DateServicesTests(TestCase):
+    def setUp(self) -> None:
+       self.dates = ['2022-12-13', '1970-01-01', '2011-13-122', '2011:13:12']
+       self.datetimes = ['2022-12-13T13:24', '1970-01-01T01:56',
+                         '2022-12-13T13:78', '1970-01-01K01:56']
+
+    def test_get_date_from_str(self):
+        self.assertEqual(getDateFromStr(self.dates[0]), date(2022, 12, 13)) 
+        self.assertEqual(getDateFromStr(self.dates[1]), date(1970, 1, 1)) 
+        with self.assertRaises(Exception):
+            date_obj = getDateFromStr(self.dates[2])
+            date_obj = getDateFromStr(self.dates[3])
+    
+    def test_get_datetime_from_str(self):
+        self.assertEqual(getDateAndTimeFromStr(self.datetimes[0]), datetime(2022, 12, 13, 13, 24))
+        self.assertEqual(getDateAndTimeFromStr(self.datetimes[1]), datetime(1970, 1, 1, 1, 56))
+        with self.assertRaises(Exception):
+            date_time = getDateAndTimeFromStr(self.datetimes[2])
+            date_time = getDateAndTimeFromStr(self.datetimes[3])
+
+
+
+    
+
+
+        
+        
--- journal/users/tests.py
+++ journal/users/tests.py
@@ -1,3 +1,61 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from users.teacher_services import addNewTeacherToDatabase, deleteTeacherFromDatabase, getTeacher, updateExistingTeacher, validateTeacherData
+
+from .models import Teacher
+
+class TeacherServicesTests(TestCase):
+    def setUp(self) -> None:
+       Teacher.objects.create(surname='Иванов', name='Иван',
+                              patronymic='Иванович', military_rank='капитан',
+                              military_post='преподаватель', cycle='Цикл 1',
+                              role=1, login='ivanov_i_i', password='t12345678',
+                              status=True)
+       self.input_data_correct = {'surname': 'Сидоров', 'name': 'Петр', 'patronymic': 'Сергеевич',
+                    'military_post': 'старший преподаватель', 'military_rank': 'подполковник', 'cycle': 'Цикл 2',
+                    'role': 1, 'login': 'sidorov_p_s', 'password': 'oiu564123'}
+       self.input_data_incorrect = {'surname': 'Сидоров', 'name': 'Петр', 'patronymic': 'Сергеевич',
+                    'military_post': 'csdsdv', 'military_rank': 'ddsfsdfa', 'cycle': 'Цикл 2',
+                    'role': 1, 'login': 'sidorov_p_s', 'password': 'oiu564123'}
+       self.input_data_updating = {'surname': 'Иванов', 'name': 'Иван', 'patronymic': 'Иванович',
+                    'military_post': 'преподаватель', 'military_rank': 'майор', 'cycle': 'Цикл 2',
+                    'role': 1, 'login': 'ivanov_i_i', 'password': 't12345678'}
+
+
+    def test_validated_data(self):
+        validated_data1 = validateTeacherData(self.input_data_correct)
+        self.assertEqual(validated_data1, self.input_data_correct)
+        with self.assertRaises(ValidationError):
+            validated_data2 = validateTeacherData(self.input_data_incorrect)
+
+
+    def test_insert_teacher(self):
+        validated_data = validateTeacherData(self.input_data_correct)
+        addNewTeacherToDatabase(validated_data)
+        teacher = Teacher.objects.get(surname='Сидоров')
+        self.assertIsNotNone(teacher)
+
+
+    def test_update_teacher(self):
+        teacher = getTeacher(1)
+        self.assertIsNotNone(teacher)
+        self.assertEqual(teacher.military_rank, 'капитан')
+        validated_data = validateTeacherData(self.input_data_updating)
+        updateExistingTeacher(validated_data, 1)
+        teacher = getTeacher(1)
+        self.assertIsNotNone(teacher)
+        self.assertEqual(teacher.military_rank, 'майор')
+
+
+    def test_delete_teacher(self):
+        teacher = getTeacher(1)
+        self.assertIsNotNone(teacher)
+        deleteTeacherFromDatabase(1)
+        teacher = getTeacher(1)
+        self.assertFalse(teacher.status)
+
+
+
+
+
